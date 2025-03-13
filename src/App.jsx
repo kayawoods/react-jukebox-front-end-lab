c95e7b0e735d4651f1429c570f95dce01be99dc8
Kaya Woods <kayanevada@gmail.com>
2025-03-13 07:55:44 -0700

    update function

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,95 @@
 import { useState, useEffect } from 'react';
-import * as trackService from './services/trackService';
-import TrackList from './components/TrackList/TracktList.jsx';
+import './App.css'
+import * as trackService from './services/trackService'
+import TrackList from './components/TrackList/TrackList';
+import TrackDetail from './components/TrackDetail/TrackDetail';
+import TrackForm from './components/TrackForm/TrackForm';
 
 const App = () => {
   const [tracks, setTracks] = useState([]);
+  const [selected, setSelected] = useState(null)
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
   useEffect(() => {
     const fetchTracks = async () => {
       try {
         const fetchedTracks = await trackService.index();
+        // Don't forget to pass the error object to the new Error
         if (fetchedTracks.err) {
           throw new Error(fetchedTracks.err);
         }
         setTracks(fetchedTracks);
       } catch (err) {
+        // Log the error object
         console.log(err);
       }
     };
     fetchTracks();
   }, []);
 
+  const handleSelect = (track) => {
+    setSelected(track)
+    setIsFormOpen(false);
+  }
+
+  const handleFormView = (track) => {
+    if (!track._id) setSelected(null);
+    setIsFormOpen(!isFormOpen);
+  };
+
+  const handleAddTrack = async (formData) => {
+    try {
+      // Call petService.create, assign return value to newPet
+      const newTrack = await trackService.create(formData);
+
+      if (newTrack.err) {
+        throw new Error(newTrack.err);
+      }
+      setTracks([newTrack, ...tracks]);
+      setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+ 
+  const handleUpdateTrack = async (formData, trackId) => {
+    try {
+      const updatedTrack = await trackService.update(formData, trackId);
+  
+      if (updatedTrack.err) {
+        throw new Error(updatedTrack.err);
+      }
+  
+      const updatedTrackList = tracks.map((track) => (
+        track._id !== updatedTrack._id ? track : updatedTrack
+      ));
+      setTracks(updatedTrackList);
+      // If we don't set selected to the updated pet object, the details page will
+      // reference outdated data until the page reloads.
+      setSelected(updatedTrack);
+      setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
-      <TrackList tracks={tracks} />
+      <TrackList 
+      tracks={tracks} 
+      handleSelect={handleSelect}
+      handleFormView={handleFormView}
+      isFormOpen={isFormOpen}
+      />
+      {isFormOpen ? (
+      <TrackForm handleAddTrack={handleAddTrack} selected={selected} handleUpdateTrack={handleUpdateTrack}/>
+    ) : (
+      <TrackDetail selected={selected} handleFormView={handleFormView} />
+    )}
     </>
   );
 }
+
+
 export default App;
---
+++ src/components/TrackDetail/TrackDetail.jsx
@@ -0,0 +1,42 @@
+const TrackDetail = (props) => {
+  // return if props.selected is null
+  if (!props.selected) {
+    return (
+      <div>
+        <h1>NO DETAILS</h1>
+      </div>
+    );
+  }
+
+  // return statement if props.selected has a truthy value
+  return (
+    <div>
+      <h2>Title: {props.selected.title}</h2>
+      <h2>Artist: {props.selected.artist}</h2>
+      <button onClick={() => props.handleFormView(props.selected)}>
+        Edit Track
+      </button>
+    </div>
+  );
+};
+
+
+
+export default TrackDetail; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
